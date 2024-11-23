f443f6a5045d1cfb840c4c7d66c88e1f60c2ffbf
MOON_KI_UP <mgumjh6@naver.com>
2024-11-23 17:13:58 +0900

    변경사항 일부 적용, 서버 ,클라이언트 작동 구현중

---
+++ src/Client/MainProgram.java
@@ -0,0 +1,19 @@
+package Client;
+
+import java.util.ArrayList;
+
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+
+
+
+
+        String serverAddress = "localhost";
+        int serverPort = 54321;
+
+        NPMainPage nmp = new NPMainPage(serverAddress, serverPort);
+
+    }
+}
---
+++ src/Client/User.java
@@ -0,0 +1,39 @@
+package Client;
+
+import javax.swing.ImageIcon;
+
+public class User {
+
+	private String nickname;
+	private int score = 0;
+
+	private String ImgPath = "images/moremi.png";
+
+	// 기본 사용자 이미지 사용
+	private ImageIcon profile_pic = new ImageIcon(ImgPath);
+	public int left_time = 500; //타이머바 사이즈 가로가 500이라 50초
+
+	// 점수 세팅
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	// 프로필 이미지 세팅
+	public ImageIcon getProfilePic() {
+		return profile_pic;
+	}
+	public void setProfilePic(ImageIcon pic) {
+		this.profile_pic = pic;
+	}
+
+	// 닉네임 설정
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+}
